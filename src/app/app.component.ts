5de57b5afd26021515dc19f29af18b723dc59a68
alexandresouva <pseudoalexandre@gmail.com>
2024-05-18 22:44:32 -0300

    feat(player): implementing roll dice feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Player } from './models/player.model';
+import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  diceRoll$ = new BehaviorSubject<number | null>(null);
+  private _unsubscribe$ = new Subject<void>();
+
+  isPlaying = true;
+  private _activePlayer!: Player;
+
   player1: Player = {
     id: 1,
     name: 'Player 1',
@@ -24,4 +31,37 @@ export class AppComponent {
     isActive: false,
     isWinner: false,
   };
+
+  ngOnInit(): void {
+    this._activePlayer = this.player1;
+
+    this.diceRoll$.pipe(takeUntil(this._unsubscribe$)).subscribe((diceRoll) => {
+      if (!diceRoll) return;
+
+      if (diceRoll !== 1) {
+        this._activePlayer.currentScore += diceRoll;
+      } else {
+        this._switchActivePlayer();
+      }
+    });
+  }
+
+  rollDice() {
+    if (this.isPlaying) {
+      const diceRoll = Math.trunc(Math.random() * 6) + 1;
+      this.diceRoll$.next(diceRoll);
+    }
+  }
+
+  private _switchActivePlayer() {
+    this._activePlayer.isActive = false;
+    this._activePlayer =
+      this._activePlayer === this.player1 ? this.player2 : this.player1;
+    this._activePlayer.isActive = true;
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribe$.next();
+    this._unsubscribe$.complete();
+  }
 }
